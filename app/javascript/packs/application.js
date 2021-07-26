e4bdb83d995eec096a557ffc278affb7ff15fd8e
egor-vlas <vlasenkoiegor2021@gmail.com>
2021-07-26 17:29:49 +1000

    add gems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,15 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap"
+import '../stylesheets/application'
+
+document.addEventListener("turbolinks:load", () => {
+	$('[data-toggle="tooltip"]').tooltip()
+	$('[data-toggle="popover"]').popover()
+
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
